b261d38bbf2c5abd05f9c76c0691fe8fed91c1be
melou-dev <ameliecuaces.dev@gmail.com>
2019-09-22 17:07:15 +0200

    test done to add item at the end of the to-do-list delivery4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 `use strict`;
 
-const userInput = document.body.querySelector(`#inputUser`).createTextNode(input.value);
+const userInput = document.body.querySelector(`#inputUser`);
 const button = document.body.querySelector(`button`);
 const ul = document.body.querySelector(`ul`);
 let li;
@@ -12,6 +12,7 @@ let li;
 // déclarer les variables et les tester - ok
 // créer les fonctions vides, nommer des verbes d'action - ok
 // remplir mes fonctions et tester la sortie (console.log) - 
+//  - tester les 3ére fonctions pour insérer une tâche à la fin de la liste.
 // tester une fonction globale
 // cabler les fonctions (clavier/sourie)
 // finitions : option complémentaire
@@ -22,9 +23,8 @@ function createNewli() {
 }
 
 // fonction récupérer l'input et copier dans le LI créé.
-function copyInputValueIntoNewLI() {
- li.append( userInput );
-// li.append(document.createTextNode(input.value));
+function copyInputValueIntoNewLi() {
+li.append(document.createTextNode( userInput.value ));
 }
 
 // fonction insérer le Li et son text à la fin de la liste.
@@ -46,7 +46,11 @@ ul.insertAdjacentElement( "beforeend", li );
 
 //function removeImputValueValidated() {}
 
-//function RunMyTodolist() {}
+//function RunMyTodolist() {
+//  function createNewli();
+//  function copyInputValueIntoNewLi();
+//  function insertNewElementLiAtEnd();
+//}
 
 
 
